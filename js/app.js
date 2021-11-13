512872d4d711c2d619a6293b7edc4b5858eddc6b
zac234 <zacbenincasa@gamil.com>
2021-11-13 10:18:15 -0700

    created 'cards' using js have not put any info on them but will do so

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-document.getElementById("header_1")
+document.getElementById(header_1)
+document.getElementById(text2)
+document.getElementById(youtube)
+var header_1 = "this is a header"
+var text2 = "this is a numer of letters"
+var youtube = "this is a external link"
